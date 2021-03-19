a72c9940693ea545741c462d4fb22262e6a2c637
jotheeswaran-dev <79405572+jotheeswaran-dev@users.noreply.github.com>
2021-03-19 18:32:58 +0530

    Handson submission

--- data-binding-handson/src/app/app.component.ts
+++ data-binding-handson/src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-binding-handson';
+    username = '';
+    disableUser = false;
+
+    constructor() {
+        this.disableUsername();
+    }
+
+    addUser(event : Event) {
+        this.username = (<HTMLInputElement>event.target).value;
+            //if (this.username.length >= 1) {
+            //this.disableUser = true;
+        //}
+    }
+
+    disableUsername() {
+        if (this.username) {
+            this.disableUser = false;
+        }
+        else {
+            this.disableUser = true;
+        }
+    }
+
+    resetUsername() {
+        this.username = '';
+    }
 }
